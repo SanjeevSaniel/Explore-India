1e90bf60e8079cff6810e98afa34b3778bad19c1
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-12-07 22:46:14 +0530

    Added Homepage video

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -12,9 +12,22 @@ const HomePage = () => {
   return (
     <div>
       <div className="bg-[url(https://images.pexels.com/photos/2314983/pexels-photo-2314983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)] bg-cover bg-bottom relative">
-        <div className="backdrop-brightness-[80%]">
+        <video
+          autoPlay
+          // controls
+          // loop
+          muted
+          className="absolute top-0 bottom-0 w-full h-full mx-auto rounded-lg shadow-xl"
+        >
+          <source
+            src="https://www.incredibleindia.org/content/dam/incredible-india-v2/video/original/Incredible_india_Edit_v10_1980x786_resolution.mp4"
+            type="video/mp4"
+          />
+        </video>
+
+        <div className="backdrop-brightness-[70%] z-10">
           <NavBar />
-          <div className="grid grid-cols-2 max-w-[1280px] h-[32rem] m-auto text-white">
+          <div className="grid grid-cols-2 lg:max-w-[1280px] lg:h-[31.8rem] m-auto text-white">
             <section className="pt-[5rem]">
               <p className="text-[4rem] font-bold leading-[4.5rem]">
                 Travel memories you&apos;ll never forget
