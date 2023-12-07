eff60042e36564a833b14bea5d66ad822a290bf4
Sanjeev Kujur <sanjeevsaniel@outlook.in>
2023-12-07 20:30:27 +0530

    Removed Navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,9 @@
 import { Outlet } from "react-router-dom";
-import NavBar from "./components/Header/NavBar";
 import Footer from "./components/Footer/Footer";
 
 const Layout = () => {
   return (
     <div>
-      {/* Layout */}
-      <NavBar />
       <Outlet />
       <Footer />
     </div>
